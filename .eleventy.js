64c22b31ae6e6c240fd0493ba4d82a7801087dc3
Daniel Beutner <daniel@beutner.dev>
2021-12-20 14:04:18 +0100

    build(swc): switches to swc / spack

--- .eleventy.js
+++ .eleventy.js
@@ -1,50 +1,38 @@
 const atImport = require("postcss-import")
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-const esbuild = require('esbuild');
 const fs = require('fs/promises');
 const postcss = require('postcss');
 const postcssNested = require('postcss-nested');
 const swc = require('@swc/core')
 const { config } = require('@swc/core/spack');
-const { dirname } = require("path");
 
-const TRANSFORM = 'esbuild';
-
-async function transformJs(entryFile, outputFile) {
+async function transformJs(outputPath) {
     try {
-        if (TRANSFORM === 'esbuild') {
-            await esbuild.build({
-                entryPoints: [entryFile],
-                bundle: true,
-                minify: process.env.ELEVENTY_ENV === "production",
-                sourcemap: process.env.ELEVENTY_ENV !== "production",
-                target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
-                outfile: outputFile,
-            })
-        } else {
-            const outputPath = dirname(outputFile);
-            const publicFolderExists = (await fs.stat(outputPath)).isDirectory()
-            const spackConfig = config({
-                entry: {
-                    main: entryFile,
-                },
-                output: {
-                    path: outputPath
-                },
-                module: {},
-            })
-            const bundle = await swc.bundle(spackConfig);
+        const publicFolderExists = (await fs.stat(outputPath)).isDirectory()
+        const spackConfig = config({
+            mode: process.env.ELEVENTY_ENV === 'production' ? 'production' : 'none',
+            target: 'browser',
+            entry: {
+                main: __dirname + '/src/scripts/main.ts',
+            },
+            output: {
+                path: outputPath
+            },
+            module: {},
+        })
+        const bundle = await swc.bundle(spackConfig);
 
-            if (!publicFolderExists) {
-                await fs.mkdir(outputPath);
-            }
-
-            Object.keys(spackConfig.entry).forEach(async key => {
-                await fs.writeFile(`${outputPath}/${key}.js`, bundle[key].code);
-                await fs.writeFile(`${outputPath}/${key}.map.js`, bundle[key].map);
-            })
+        if (!publicFolderExists) {
+            await fs.mkdir(outputPath);
         }
+
+        Object.keys(spackConfig.entry).forEach(async key => {
+            const fileName = `${key}.js`;
+            
+            await fs.writeFile(`${outputPath}/${fileName}`, bundle[key].code, 'utf-8');
+            await fs.writeFile(`${outputPath}/${fileName}.map`, bundle[key].map, 'utf-8');
+        })
     } catch (error) {
         console.error(error);
     }
@@ -56,11 +44,11 @@ async function transformCss(entryFile, outputFile) {
         const transformed = await postcss([atImport, autoprefixer, postcssNested, cssnano])
             .process(css, { from: entryFile, to: outputFile })
         if ('css' in transformed && transformed.css) {
-            await fs.writeFile(outputFile, transformed.css)
+            await fs.writeFile(outputFile, transformed.css, 'utf-8')
         }
 
         if ('map' in transformed && transformed.map) {
-            await fs.writeFile(`${outputFile}.map`, transformed.map.toString())
+            await fs.writeFile(`${outputFile}.map`, transformed.map.toString(), 'utf-8')
         }
     } catch (error) {
         console.error(error)
@@ -69,7 +57,7 @@ async function transformCss(entryFile, outputFile) {
 
 module.exports = function (config) {
     config.on('afterBuild', async () => {
-        await transformJs(__dirname + '/src/scripts/main.ts', __dirname + '/public/main.js')
+        await transformJs(__dirname + '/public')
         await transformCss(__dirname + '/src/styles/main.css', __dirname + '/public/main.css');
     });
 
