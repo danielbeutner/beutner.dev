5477346fad7d711d10959b539f4f1f679df51536
Daniel Beutner <daniel@beutner.dev>
2022-02-17 16:35:56 +0100

    build(scripts): use esbuild for transpilation

--- src/assets/scripts/_scripts.11ty.js
+++ src/assets/scripts/_scripts.11ty.js
@@ -1,45 +1,47 @@
-const swc = require('@swc/core');
-const { config } = require('@swc/core/spack');
-const { getSize, createDebug } = require('../../../utils/helper');
+const esbuild = require('esbuild');
+const { getSize } = require('../../../utils/helper');
 
-const debug = createDebug('scripts');
 const OUTPUT_FILE_PATH = '/assets/scripts/main.js';
+const ENTRY_POINT = __dirname + '/main.ts';
 const isProd = process.env.ELEVENTY_ENV === 'production';
 
 class Scripts {
   data() {
-    const spackConfig = config({
-      mode: isProd ? 'production' : 'none',
-      target: 'browser',
-      entry: {
-        main: __dirname + '/main.ts',
-      },
-      module: {},
-    });
+    const config = {
+      entryPoints: [ENTRY_POINT],
+      bundle: true,
+      minify: isProd,
+      sourcemap: isProd,
+      target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+      outfile: OUTPUT_FILE_PATH,
+      write: false,
+    };
 
     return {
       permalink: OUTPUT_FILE_PATH,
       eleventyExcludeFromCollections: true,
-      spackConfig,
+      config,
     };
   }
 
-  async transform(spackConfig) {
-    const output = await swc.bundle(spackConfig);
+  async build(config) {
+    const output = await esbuild.build(config);
 
     return output;
   }
 
-  async render({ spackConfig }) {
-    debug('bundling scripts...');
+  async render({ config }) {
+    console.log('bundling scripts...');
     try {
-      const {
-        main: { code },
-      } = await this.transform(spackConfig);
+      const result = await this.build(config);
 
-      debug('built %s (%s kB)', OUTPUT_FILE_PATH, getSize(code));
+      // We only use the first entry point as the script only
+      // supports one file at a time
+      const code = result.outputFiles[0].text;
 
-      return code;
+      console.log('built %s (%s kB)', OUTPUT_FILE_PATH, getSize(code));
+
+      return code.toString();
     } catch (error) {
       console.error(error);
       return null;
