2fee9ce9cb08efd425b982bd52cc0ae3cb8aaf86
Daniel Beutner <daniel@beutner.dev>
2022-03-04 11:46:47 +0100

    fix: remove unused var

--- utils/markdown-it-images.js
+++ utils/markdown-it-images.js
@@ -2,10 +2,9 @@ const path = require('node:path');
 const shortcodes = require('./shortcodes.js');
 
 module.exports = function images_plugin(md, mdOptions) {
-  const defaultImageRenderer = md.renderer.rules.image;
-
   md.renderer.rules.image = (tokens, idx, options, env, self) => {
     if (env.page.inputPath === undefined) return;
+
     const { dir: sourceDir } = path.parse(env.page.inputPath);
     const token = tokens[idx];
     const [, srcFileName] = token.attrs.find(([key]) => key === 'src');
